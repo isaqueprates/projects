63b60df99d5b69f835f75633394b73e5dfe2ce4c
isaqueprates <59794601+isaqueprates@users.noreply.github.com>
2023-05-25 00:33:53 -0300

    exercício
    att de exercício

--- curso-Luiz-Otavio/exe-switch-data/assets/js/main.js
+++ curso-Luiz-Otavio/exe-switch-data/assets/js/main.js
@@ -1,40 +1,85 @@
-const day = document.querySelector("h1")
+const textTempo = document.querySelector("h1")
 
 const data1 = new Date();
 const diaSemana = data1.getDay()
+const mesAno = data1.getMonth()
+const opcoes = {
+    dateStyle: 'full',
+    timeStyle:'short'
+};
 
-day.innerText = verificarDia(diaSemana)
-myDate()
-function myDate (){
-    // let data = new Date();
-    // let dia = data.toDateString()
-    // console.log(data.toLocaleDateString())
-    // console.log(dia)
-}
+textTempo.innerText = data1.toLocaleString('pt-BR', opcoes)
 
-function verificarDia (dia){
-    switch(dia){
-        case 0:
-            console.log('Domingo')
-        break;
-        case 1:
-            console.log('Segunda-feira')
-        break;
-        case 2:
-            console.log('Terça-feira')
-        break;
-        case 3:
-            console.log('Quarta-feira')
-        break;
-        case 4:
-            console.log('Quinta-feira')
-        break;
-        case 5:
-            console.log('Sexta-feira')
-        break;
-        case 6:
-            console.log('Sabado')
-        break;
-        default:
-    }
-}
+/******--------Jeito mais complicado--------******/
+// textTempo.innerText = `${verificarDia(diaSemana)}  ${data1.getDate()} de ${verificarMes(mesAno)} ${data1.getHours()}:${data1.getMinutes()}`
+
+// function verificarDia (day){
+//     let dayName;
+//     switch(day){
+//         case 0:
+//             dayName = 'Domingo';
+//             return dayName;
+//         case 1:
+//             dayName = 'Segunda-feira';
+//             return dayName;
+//         case 2:
+//             dayName = 'Terça-feira';
+//             return dayName;
+//         case 3:
+//             dayName = 'Quarta-feira';
+//             return dayName;
+//         case 4:
+//             dayName = 'Quinta-feira';
+//             return dayName;
+//         case 5:
+//             dayName = 'Sexta-feira';
+//             return dayName;
+//         case 6:
+//             dayName = 'Sabado';
+//             return dayName;
+//         default:
+//     }
+// }
+
+// function verificarMes (mes){
+//     let mesName;
+//     switch(mes){
+//         case 0:
+//             mesName = 'Janeiro';
+//             return mesName;
+//         case 1:
+//             mesName = 'Fevereiro';
+//             return mesName;
+//         case 2:
+//             mesName = 'Março';
+//             return mesName;
+//         case 3:
+//             mesName = 'Abril';
+//             return mesName;
+//         case 4:
+//             mesName = 'Maio';
+//             return mesName;
+//         case 5:
+//             mesName = 'Junho';
+//             return mesName;
+//         case 6:
+//             mesName = 'Julho';
+//             return mesName;
+//         case 7:
+//             mesName = 'Agosto';
+//             return mesName;
+//         case 8:
+//             mesName = 'Setembro';
+//             return mesName;
+//         case 9:
+//             mesName = 'Outubro';
+//             return mesName;
+//         case 10:
+//             mesName = 'Novembro';
+//             return mesName; 
+//         case 11:
+//             mesName = 'Dezembro';
+//             return mesName;   
+//         default:
+//     }
+// }
